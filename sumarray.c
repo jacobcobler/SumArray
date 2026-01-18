ceb56365658d57612e35038d2a564fa9f6424635
jacobcobler <jxcobler@gmail.com>
2026-01-18 03:11:11 +0000

    Sum of Arrays file written

--- sumarray.c
+++ sumarray.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    int sum = 0;
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (int i = 0; i < 10; i++) {
+        sum = sum + arr[i];
+    }
+
+    printf("%i\n", sum);
     return 0;
 }
