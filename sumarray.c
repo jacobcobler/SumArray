df418400a526270fb3e283ad41c81bf1df403c24
jacobcobler <jxcobler@gmail.com>
2026-01-18 21:08:09 +0000

    Wrote the array to print along with the sum

--- sumarray.c
+++ sumarray.c
@@ -4,10 +4,21 @@ int main() {
     int sum = 0;
     int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    printf("Array: {");
+    for (int i = 0; i < 10; i++) {
+        if(i == 0) {
+            printf("%i,", arr[i]);
+        } else if (i == 9) {
+            printf(" %i}\n", arr[i]);
+        } else {
+            printf(" %i,", arr[i]);
+        }
+    }
+
     for (int i = 0; i < 10; i++) {
         sum = sum + arr[i];
     }
+    printf("Sum: %i\n", sum);
 
-    printf("%i\n", sum);
     return 0;
 }
